db8aa0368370fe52625bf29e18f2050277ff6649
Kuba <Kuba@Kuba-Komputer>
2018-05-01 19:52:25 +0200

    zadanie 4 i 5

---
+++ JavaApplication56/src/Zadania/Zad4i5/FiguraGeometryczna.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Zadania.Zad4i5;
+
+/**
+ *
+ * @author Kuba
+ */
+public class FiguraGeometryczna {
+    
+}
